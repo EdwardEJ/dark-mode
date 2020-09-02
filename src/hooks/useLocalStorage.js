11d5b53f62fce7a7a12b1eeee7f981049068d6a7
A <jesus.e.estrada@hotmail.com>
2020-09-02 17:26:37 -0500

    created useLocalStorage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (JSON.parse(window.localStorage.getItem(key))) {
+      return (JSON.parse(window.localStorage.getItem(key)))
+    } else {
+      window.localStorage.setItem(key, JSON.stringify(initialValue))
+      return initialValue
+    }
+  })
+  return [storedValue]
+
+}
